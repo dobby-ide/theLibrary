ac89b353f05e540f314264cfc88cbe7d1e8e237f
Fabio Privitera <fabio_privitera@yahoo.it>
2023-04-01 19:37:19 +0300

    adds list of books from same author in SearchBookDetail

--- src/pages/searchBook/SearchBookDetail.tsx
+++ src/pages/searchBook/SearchBookDetail.tsx
@@ -2,13 +2,34 @@ import { useParams } from 'react-router-dom'
 
 import classes from './styling/SearchBookDetail.module.scss'
 import Book from '../../model/book'
+import { AuthorBookTable } from './utilComponents/AuthorBookTable'
+import { RootState } from '../../store'
+import { useSelector } from 'react-redux'
 
-const SearchBookDetail = (props:{ book:Book[] }) => {
+const SearchBookDetail = (props: { book: Book[] }) => {
   const params = useParams()
   const [filteredResult] = props.book.filter((book) => String(book.ISBN) === String(params.bookID))
+  const books = useSelector((state: RootState) => state.book.Books)
+  const author = filteredResult.authors[0].name
+  const booksOfAuthor = books.filter((book) => book.authors[0].name === author)
+  const data = booksOfAuthor.map((book) => {
+    const { title, authors, ISBN } = book
+    return {
+      title: book.title,
 
+      author: author,
+      year: 222,
+      reviews: {
+        positive: 2,
+        negative: 2
+      }
+    }
+  })
   return (
     <section className={classes.bookDetail_container}>
+      <div className={classes.moreAboutTheAuthor}>
+        <AuthorBookTable data={data}></AuthorBookTable>
+      </div>
       <div className={classes.card}>
         <div className={classes.card__side_front}>
           <h1>{filteredResult.title}</h1>
